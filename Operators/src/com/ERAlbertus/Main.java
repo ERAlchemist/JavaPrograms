79c0210209512f55fd288ef7cdf5cbad1f425e94
Eric Albertus <ERAlchemist@gmail.com>
2018-07-15 23:03:17 -0700

    more lessons completed

--- Operators/src/com/ERAlbertus/Main.java
+++ Operators/src/com/ERAlbertus/Main.java
@@ -70,5 +70,30 @@ public class Main {
         int secondTopScore = 60;
         if(topScore > secondTopScore && topScore < 100)
             System.out.println("Greater than second top score and less than 100");
+
+        double newValue = 50;
+        if(newValue == 50)
+            System.out.println("This is true");
+
+        boolean isCar = false;
+        if(isCar = true)
+            System.out.println("This is not supposed to happen");
+
+        boolean wasCar = isCar ? true : false;
+        if(wasCar)
+            System.out.println("wasCar is true");
+
+        // 1. Create a double variable with the value of 20
+        double myDouble = 20;
+        // 2. Create a second variable of type double with the value of 80
+        double secondDouble = 80;
+        // 3. Add both numbers up and multiply by 25
+        double sum =  25*(myDouble + secondDouble);
+        // 4. Use the remainder operator to figure out the remainder from the sum of #3 divided by 40
+        double remainder =  sum%40;
+        // 5. Write an "if" statement that displays a message "Total was over the limit"
+        // if the remaining total (#4) is equal to 20 or less.
+        if(remainder <= 20)
+            System.out.println("Total was over the limit");
     }
 }
