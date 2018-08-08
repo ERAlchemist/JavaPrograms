f3fe6d27b95eb1ecf9d1e3c79108eedae2878a7a
Eric Albertus <ERAlchemist@gmail.com>
2018-08-07 19:49:14 -0700

    Autoboxing and Unboxing

---
+++ AutoboxingAndUnboxing/src/com/ERAlbertus/Main.java
@@ -0,0 +1,41 @@
+package com.ERAlbertus;
+
+import java.util.ArrayList;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        String [] strArray = new String[10];
+        int [] intArray = new int[10];
+
+        ArrayList<String> strArrayList = new ArrayList<String>();
+        strArrayList.add("Tim");
+
+        ArrayList<Integer> intArrayList = new ArrayList<Integer>();
+        for(int i=0; i<=10; i++){
+            intArrayList.add(Integer.valueOf(i));
+        }
+        for(int i = 0; i<=10; i++){
+            System.out.println(i + " --> " + intArrayList.get(i).intValue());
+        }
+
+        Integer myIntValue = 56; // Integer.valueOf(56);
+
+        int myInt = myIntValue; // myInt.intValue();
+
+        //Autoboxing
+        ArrayList<Double> myDoubleValues = new ArrayList<Double>();
+        for(double dbl=0.0; dbl<=10.0; dbl += 0.5){
+            myDoubleValues.add(Double.valueOf(dbl));
+        }
+
+        //Unboxing
+        for(int i=0; i<myDoubleValues.size(); i++) {
+            double value = myDoubleValues.get(i).doubleValue();
+            System.out.println(i + " --> " + value);
+
+        }
+    }
+}
