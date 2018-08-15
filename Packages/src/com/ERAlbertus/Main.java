72e37de1e64958045f5e1d68644abdc0c8f983e9
Eric Albertus <ERAlchemist@gmail.com>
2018-08-14 23:04:27 -0700

    packages

---
+++ Packages/src/com/ERAlbertus/Main.java
@@ -0,0 +1,11 @@
+package com.ERAlbertus;
+
+import javafx.scene.Node;
+import org.w3c.dom.Node;
+public class Main {
+
+    public static void main(String[] args) {
+
+        javafx.scene.Node node = null;
+    }
+}
