746efc76462a5132c116d4cac6dbb013292f45b4
Eric Albertus <ERAlchemist@gmail.com>
2018-08-19 20:15:45 -0700

    SetAndHashSet challenge

---
+++ SetAndHashSet/src/com/ERAlbertus/DwarfPlanet.java
@@ -0,0 +1,8 @@
+package com.ERAlbertus;
+
+public class DwarfPlanet extends HeavenlyBody {
+
+    public DwarfPlanet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+}
