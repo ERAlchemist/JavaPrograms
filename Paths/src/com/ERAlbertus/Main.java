1b94a1f042d1f606467cb280957d1fff80ca01d8
Eric Albertus <ERAlchemist@gmail.com>
2018-09-09 23:04:35 -0700

    Paths

---
+++ Paths/src/com/ERAlbertus/Main.java
@@ -0,0 +1,68 @@
+package com.ERAlbertus;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+            Files.deleteIfExists(fileToDelete);
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples" , "file1.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples",  "file2.txt");
+//            Files.move(fileToMove, destination);
+//            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//
+//            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
+//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+//	    Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+//	    printFile(path);
+//	   // Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
+//        Path filePath = Paths.get(".", "files", "SubdirectoryFile.txt");
+//	    printFile(filePath);
+//	    filePath = Paths.get("/home/e/Desktop/JavaPrograms/OutThere.txt");
+//	    printFile(filePath);
+//
+//	    filePath = Paths.get(".");
+//        System.out.println(filePath.toAbsolutePath());
+//
+//        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "SubdirectoryFile.txt");
+//        System.out.println(path2.normalize().toAbsolutePath());
+//        printFile(path2.normalize());
+//
+//        Path path3 = FileSystems.getDefault().getPath("thisfiledoesnotexist.txt");
+//        System.out.println(path3.toAbsolutePath());
+//
+//        Path path4 = Paths.get("/home/e/Desktop", "JavaPrograms", "Whatever.txt");
+//
+//        System.out.println(path4.toAbsolutePath());
+//
+//        filePath = FileSystems.getDefault().getPath("files");
+//        System.out.println("Exists = " + Files.exists(filePath));
+//
+//        System.out.println("Exists = " + Files.exists(path4));
+//    }
+
+    private static void printFile(Path path) {
+        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+            String line;
+            while((line = fileReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
