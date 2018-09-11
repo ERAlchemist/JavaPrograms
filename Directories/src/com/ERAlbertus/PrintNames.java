c716c770decb95172900d77e331be49a6721eb6a
Eric Albertus <ERAlchemist@gmail.com>
2018-09-10 19:54:14 -0700

    Directories

---
+++ Directories/src/com/ERAlbertus/PrintNames.java
@@ -0,0 +1,28 @@
+package com.ERAlbertus;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class PrintNames extends SimpleFileVisitor<Path> {
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println(file.toAbsolutePath());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.println(dir.toAbsolutePath());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException e) throws IOException {
+        System.out.println("Error accessing file : " + file.toAbsolutePath() + " " + e.getMessage());
+        return FileVisitResult.CONTINUE;
+    }
+}
